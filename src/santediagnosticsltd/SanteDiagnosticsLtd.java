c957da667f933ad88c3174b3765926a26d5fe3b3
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-06-06 16:54:30 +0100

    Initial baseline: Netbeans JavaFx template generated

---
+++ src/santediagnosticsltd/SanteDiagnosticsLtd.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package santediagnosticsltd;
+
+/**
+ *
+ * @author Emmanuel Abbah
+ */
+public class SanteDiagnosticsLtd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
